98bc4f0a277e722865d0dc1a63ecfb437345afe3
Romel Indemne <romel.i@gagroup.net>
2022-11-11 10:18:48 +0400

    updated api url

--- client/src/components/posts/PostComponent.vue
+++ client/src/components/posts/PostComponent.vue
@@ -5,6 +5,8 @@
                 <div>{{ post.text }}</div>
             </div>
         </div>
+        <button type="button" @click="getLocalPosts">Get local posts</button>
+        <button type="button" @click="getPosts">Get live posts</button>
     </div>
 </template>
   
@@ -21,19 +23,25 @@ export default {
         }
     },
     methods: {
-        async getPosts() {
+        async getLocalPosts() {
             const response = await axios.get('http://localhost:5000/api/posts');
+            console.log("response", response);
+            this.posts = response.data;
+        },
+        async getPosts() {
+            const response = await axios.get('http://mevn-groundup.melinfinity.com/api/posts');
             console.log("response", response);
             this.posts = response.data;
         }
     },
     created() {
-        this.getPosts();
+        console.log('created');
     }
 }
 </script>
   
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
   
